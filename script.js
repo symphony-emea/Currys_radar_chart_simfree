5126c9a23909d59a9a52cf41e81f1dd3fa490bee
Mahesh Kumar <129132367+symphony-emea@users.noreply.github.com>
2023-07-21 14:55:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ const ctx = document.getElementById('myChart');
 
 const data = {
     labels: [
-        'Apple iPhone 14',
-        'Apple iPhone 14 Plus',
-        'Apple iPhone 14 Pro',
-        'Apple iPhone 14 Pro Max',
-        'Apple iPhone SE (2022)',
-        'Google Pixel 6a',
-        'Google Pixel 7',
-        'Google Pixel 7 Pro',
-        'Google Pixel 7a',
-        'Samsung Galaxy A34 5G',
-        'Samsung Galaxy A54 5G',
-        'Samsung Galaxy S23',
-        'Samsung Galaxy S23 Ultra',
-        'Samsung Galaxy S23+',
-        'Samsung Galaxy Z Flip4',
-        'Samsung Galaxy Z Fold4',
+        'i14',
+        'i14 Plus',
+        'i14 Pro',
+        'i14 Pro Max',
+        'iSE (2022)',
+        'P6a',
+        'P7',
+        'P7 Pro',
+        'P7a',
+        'Galaxy A34 5G',
+        'Galaxy A54 5G',
+        'S23',
+        'S23 Ultra',
+        'S23+',
+        'Z Flip4',
+        'Z Fold4',
     ],
     datasets: [
         {
-            label: 'DCD',
+            label: 'DCD%',
             data: [49, 47, 45, 48, 66, 91, 85, 71, 48, 55, 42, 27, 19, 22, 29, 23],
             // fill: true,
             backgroundColor: 'rgba(255, 99, 132, 0.2)',
@@ -32,7 +32,7 @@ const data = {
             // pointHoverBorderColor: 'rgb(255, 99, 132)'
         },
         {
-            label: 'CB',
+            label: 'CB%',
             data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 25, 39, 9, 16, 14, 10, 17],
             // fill: true,
             backgroundColor: 'rgba(54, 162, 235, 0.2)',
@@ -43,7 +43,7 @@ const data = {
             // pointHoverBorderColor: 'rgb(54, 162, 235)',
         },
         {
-            label: 'TIB',
+            label: 'TIB%',
             data: [39, 41, 44, 39, 19, 0, 3, 19, 45, 7, 4, 30, 33, 29, 55, 55],
             // fill: true,
             backgroundColor: 'rgba(255, 206, 86, 0.2)',
@@ -54,7 +54,7 @@ const data = {
             // pointHoverBorderColor: 'rgb(255, 206, 86)',
         },
         {
-            label: 'GC',
+            label: 'GC%',
             data: [13, 12, 11, 13, 16, 9, 12, 9, 7, 13, 15, 34, 32, 35, 6, 5],
             // fill: true,
             backgroundColor: 'rgba(75, 192, 192, 0.2)',
@@ -97,4 +97,3 @@ new Chart(ctx, {
         }
     }
 });
-
